fa758b12f006ac80da1336f49f88894c9ca49724
dotpegaso <dotpegaso@gmail.com>
2019-03-29 19:59:40 -0300

    added header component

--- src/modules/App.js
+++ src/modules/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import '../res/scss/main.scss';
+
 import Home from './home';
 import Engine from './engine';
 import Color from './color';

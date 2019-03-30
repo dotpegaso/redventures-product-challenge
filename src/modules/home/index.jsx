b69d741e184904bdc09d1f5653b761080370ae84
dotpegaso <dotpegaso@gmail.com>
2019-03-29 22:01:13 -0300

    static desktop version of home

--- src/modules/home/index.jsx
+++ src/modules/home/index.jsx
@@ -1,12 +1,63 @@
 import React, { Component } from 'react';
 import Header from '../../components/header';
-import './style.scss';
+import style from './style.module.scss';
+import Arrow from '../../res/images/utils/arrow.svg';
 
 class Home extends Component{
     render(){
+        const model = 'R';
+
         return(
-            <section id="home">
+            <section className={style.home}>
                 <Header />
+
+                <div className={style.showcase}>
+
+                    <p className={style.intro}>Build your</p>
+                    <p className={style.modelName}>
+                        Model <span className={style.type}>{model}</span>
+                    </p>
+
+                    <figure className={style.vehicleFigure}>
+                        <img src={require('../../res/images/home/car-home.png')} alt={`Vehicle Model ${model}`}/>
+                    </figure>
+
+                    <a className={style.begin} href="/engine">
+                        Begin 
+                        <img src={Arrow} alt="Arrow pointing right"/>
+                    </a>
+
+                </div>
+
+                <footer>
+                    <div className={style.info}>
+                        <p className={style.value}>
+                            2.5 <span className={style.speedUnit}>s</span>
+                        </p>
+                        <p className={style.description}>
+                            From 0 to 100
+                        </p>
+                    </div>
+
+                    <div className={style.info}>
+                        <p className={style.value}>
+                            420 <span className={style.speedUnit}>mi</span>
+                        </p>
+                        <p className={style.description}>
+                            Miles range
+                        </p>
+                    </div>
+
+                    <div className={style.info}>
+                        <p className={style.value}>
+                            250 <span className={style.speedUnit}>mp/h</span>
+                        </p>
+                        <p className={style.description}>
+                            Max speed
+                        </p>
+                    </div>
+                </footer>
+
             </section>
         )
     }
